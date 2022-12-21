5434e9169f9fcfeded9696b5004e748f1a2ec537
SharkHera <62921543+SharkHera@users.noreply.github.com>
2022-12-21 09:43:09 -0400

    exo resolu

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 ////////// Insérez ici votre code  ///////////////////////
 
+animals = animals.sort().reverse()
+                .filter(animal => animal[0] !== "e" && animal[0] !== "m")
+                .filter(animal => !animal.includes("ll"))
 
+
+
+let search = function(searchText){
+    return animals.filter(animal => animal.includes(searchText.toLowerCase()))
+}
 
 ///////////////////////////////////////////////
